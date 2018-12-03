6675fbbefc8f3a8481c9dbeb2bd02325dcfd127b
Daniel Ruiz <D.Ruiz@t-systems.com>
2018-12-03 22:17:40 +0100

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,211 @@
+let resumeData = {
+    "name": "Daniel Ruiz",
+    "role": "Software Engineer",
+    "mail":" druizcayuela@gmail.com",
+    "roleDescription": " I have more than 7 years experience using Java languages to create and implement software applications. I enjoy being challenged and engaging with projects that require me to work outside my comfort and knowledge set, as continuing to learn new languages and development techniques are important to me and the success of any organization.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/druizcayuela/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/druizcayuela",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://skype.com",
+          "className":"fa fa-skype"
+        }
+      ],
+    "aboutme":"I am passionate about software development world, best practices, good methologics, clean code best practices, and lover of agile philosophy. I have been working in this industry for more than 7 years.",
+    "address":"Spain - Granada",
+    "education":[
+      {
+        "UniversityName":"UNED",
+        "specialization":"Computer Engineering",
+        "MonthOfPassing":"Currently",
+        "YearOfPassing":"",
+        "Achievements":"Computer Engineering is defined as the discipline that embodies the science and technology of design, construction, implementation, and maintenance of software and hardware components of modern computing systems and computer-controlled equipment."
+      },
+      {
+        "UniversityName":"APMG International",
+        "specialization":"DASA DevOps Fundamentals",
+        "MonthOfPassing":"Oct",
+        "YearOfPassing":"2018",
+        "Achievements":"The qualification is designed to provide the core education necessary to build your DevOps vocabulary and to understand its principles and practices. Badge holders will be able to describe how DevOps relates to Lean and Agile methodologies as well as being able to discuss the critical success factors for DevOps implementation."
+      },
+      {
+        "UniversityName":"ESOL Cambridge",
+        "specialization":"English B2 First Certificate",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2015",
+        "Achievements":"With the B2 First degree, you demonstrate to the world that you possess the language skills necessary to live and work independently in an English-speaking country, as well as to study courses taught in English."
+      },
+      {
+        "UniversityName":"Oracle Certified Professional",
+        "specialization":"Java SE 6 Programmer",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2015",
+        "Achievements":"An Oracle Certified Professional, Java SE 6 Programmer has experience using the Java programming language, understands the basic syntax and structure of the Java programming language and can create Java technology applications that run on server and desktop systems using J2SE 6.0."
+      },
+      {
+        "UniversityName":"IES Francisco Javier de Burgos",
+        "specialization":"Technician Computer Systems Administration",
+        "MonthOfPassing":"Jul",
+        "YearOfPassing":"2009",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Unit 4",
+        "specialization":"Software Engineering",
+        "MonthOfLeaving":"Currently",
+        "YearOfLeaving":"",
+        "Achievements":"To be filled ..."
+      },
+      {
+        "CompanyName":"T-Systems Iberia",
+        "specialization":"Analyst programmer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2019",
+        "Achievements":"Car technical documents management: creating components in React JS/Redux with custom HOC and Middlewares. Styling with Sass. The API Rest is done with Node.JS. Using middlewares such as Express, Helmet. Morgan, BodyParser ... Unit tests and Integrational tests with Jest and Enzyme package and Moxios to mock. I'm used to working with Test-Driven Development and Behavior-Driven Development as well. MongoDB/Mongoose as NoSql database. Using TypeScript and webpack. Swagger to document the endpoints.\n International project performing construction functions, analysis and code development, providing robust solutions and sharing knowledge with my team. The technology with which Iwork is based on Java (JSF / JSP / Spring) combined with browser technology (HTML5, CSS3, JavaScript).\n I work with GIT as version control, SonarQube for code quality control, and others like Jenkins for continuous integration.\n The applications are operated by T-Systems, by fulfilling the client operation requirements, based on IBM WebSphere 8. The Business Logic, including database access, is delegated to a separated backend system and is not part of the JAVA/ WebSphere coding. The access from WebSphere to the backend system is done via MQ-Business services.\n In the current project, of an international nature, we work as a team, organizing ourselves with tools such as JIRA and currently self-manage our tasks, contemplating the estimated dates and estimated efforts.\n Sometimes, due to the nature of the project, it is required to travel abroad for various technical and functional meetings. In addition, during the development, we maintain a constant feedback through videoconferences. The communication language is in English"
+      },
+      {
+        "CompanyName":"CGI",
+        "specialization":"Senior Software Developer",
+        "MonthOfLeaving":"Jul",
+        "YearOfLeaving":"2016",
+        "Achievements":"Performing maintenance of a project 100% backend. Project built with design pattern FACADE. Programming with Restful for requests to WebServices (Nácar). Using Java/Spring 3.0 (Core, Context) Jackson JSON and JAX-RS Service.\n  Development of all layers in a web application JAVA (from DAOs, DTO, Services, Controller) and the front-end JSP with JSTL, HTML5, Bootstrap, JavaScript / JQuery, JSON, AJAX. Using the MVC design pattern. Connection to SQL Server data base and realization of requests through RESTful webservices.\n IDE: Eclipse, STS, IntelliJ ..., SVN and Git to version control, Jenkins for continuous integration, and Sonar for code quality. Agile-SCRUM methodology work. Performing unit tests with JUnit.\n Accustomed to work on international projects for banking sector.\n Creating functional and technical documentation of the application through our customer requests."
+      },
+      {
+        "CompanyName":"Telefonica",
+        "specialization":"Software Developer",
+        "MonthOfLeaving":"Aug",
+        "YearOfLeaving":"2014",
+        "Achievements":"Performing maintenance of a project 100% backend. Project built with design pattern FACADE. Programming with Restful for requests to WebServices (NácarConstruction abstract specifications and technical documentation into executable code and consistently achieve quality standards in several projects, using Spring Framework (libraries: SpringMVC , Core , Context) and Hybernate for queries to the database (SQL Server 2008 ).\n Development of all layers of a JEE application (DAOs, DTO, Services, Controller) and the front-end with JSP, JSTL, HTML5 , Bootstrap , JavaScript / JQuery, Ajax , JSON . The web-application follows the MVC design pattern.\n IDE: Eclipse STS, version control and SVN -LEAN methodology Agile work. Performing unit tests with JUnit and functional with SELENIUM. Reporting with JasperReports.\n Develop an intranet web application that unifies all concepts of accounting, stock management , billing distributors; and also a web application for management of clients."
+      }
+    ],
+    "skillsDescription":"Highly skilled in designing, testing, and developing software.\n Thorough understanding of data structures and algorithms.\n Knowledgeable of back-end development best practices.\n Hands-on software troubleshooting experience.\n Experience in Object-Oriented Programming.\n Experience in international projects Working with Agile software development with Scrum.\n Proven track record of proper documentation for future maintenance and upgrades.\n I'm used to working with TDD/BDD, perform unit testing with JUnit and functional testing with Selenium, Cucumber, Gherkin, Jest and Enzyme.",
+    "portfolio":[
+      {
+        "icon":"devicon-java-plain",
+        "tooltip":"Java"
+      },
+      {
+        "icon":"devicon-javascript-plain",
+        "tooltip":"JavaScript"
+      },
+      {
+        "icon":"devicon-nodejs-plain",
+        "tooltip":"NodeJs"
+      },
+      {
+        "icon":"devicon-react-original",
+        "tooltip":"ReactJs"
+      },
+      {
+        "icon":"devicon-typescript-plain",
+        "tooltip":"Typescript"
+      },
+      {
+        "icon":"devicon-android-plain",
+        "tooltip":"Android"
+      },
+      {
+        "icon":"devicon-angularjs-plain",
+        "tooltip":"Angular"
+      },
+      {
+        "icon":"devicon-bootstrap-plain",
+        "tooltip":"Bootstrap"
+      },
+      {
+        "icon":"devicon-babel-plain",
+        "tooltip":"Babel"
+      },
+      {
+        "icon":"devicon-csharp-plain",
+        "tooltip":"C#"
+      },
+      {
+        "icon":"devicon-css3-plain",
+        "tooltip":"Css3"
+      },
+      {
+        "icon":"devicon-cucumber-plain",
+        "tooltip":"Cucumber"
+      },
+      {
+        "icon":"devicon-docker-plain",
+        "tooltip":"Docker"
+      },
+      {
+        "icon":"devicon-express-original",
+        "tooltip":"Express"
+      },
+      {
+        "icon":"devicon-git-plain",
+        "tooltip":"Git"
+      },
+      {
+        "icon":"devicon-gradle-plain",
+        "tooltip":"Gradle"
+      },
+      {
+        "icon":"devicon-heroku-original",
+        "tooltip":"Heroku"
+      },
+      {
+        "icon":"devicon-html5-plain",
+        "tooltip":"Html5"
+      },
+      {
+        "icon":"devicon-jquery-plain",
+        "tooltip":"jQuery"
+      },
+      {
+        "icon":"devicon-mocha-plain",
+        "tooltip":"Mocha"
+      },
+      {
+        "icon":"devicon-mongodb-plain",
+        "tooltip":"MongoDB"
+      },
+      {
+        "icon":"devicon-sass-original",
+        "tooltip":"Sass"
+      },
+      {
+        "icon":"devicon-swift-plain",
+        "tooltip":"Swift"
+      },
+      {
+        "icon":"devicon-visualstudio-plain",
+        "tooltip":"VisualStudio"
+      },
+      {
+        "icon":"devicon-webpack-plain",
+        "tooltip":"Webpack"
+      },
+      {
+        "icon":"devicon-intellij-plain",
+        "tooltip":"IntelliJ"
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
