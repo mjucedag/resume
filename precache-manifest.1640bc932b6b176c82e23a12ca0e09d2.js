a3fb11f56fa6c39c5f0cd0830072bddbcba913a1
Daniel Ruiz <druizcayuela@gmail.com>
2020-08-03 18:35:53 +0200

    Updates

---
+++ precache-manifest.1640bc932b6b176c82e23a12ca0e09d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "8747fc29c6bb9b159307",
+    "url": "/resume/static/js/main.8747fc29.chunk.js"
+  },
+  {
+    "revision": "735bd9cc8a6f0c24cc88",
+    "url": "/resume/static/js/1.735bd9cc.chunk.js"
+  },
+  {
+    "revision": "8747fc29c6bb9b159307",
+    "url": "/resume/static/css/main.a50a688b.chunk.css"
+  },
+  {
+    "revision": "96abd6bc0259058ebc16afe4bee58eab",
+    "url": "/resume/index.html"
+  }
+];
